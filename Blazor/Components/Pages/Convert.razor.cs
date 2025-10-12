07c36310dd0c29596578045360163261d73d264e
D4STEW143 <dastew143@mail.ru>
2025-10-12 15:36:48 +0900

    Before host

---
+++ Blazor/Components/Pages/Convert.razor.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Blazor.Components.Pages {
+	public partial class Convert {
+		string s_input { get; set; }
+		public enum Systems {
+			Binary, Octal, Decimal, Hexadecimal, Unknown
+		}
+
+		public Systems SystemDetection() {
+			string input = s_input;
+			if (string.IsNullOrEmpty(input)) { return Systems.Unknown; }
+			input = input.Trim().ToUpper();
+			if (Regex.IsMatch(input, @"^(0x)?[0-9A-F]+$")) {
+				if (input.StartsWith("0X") || Regex.IsMatch(input, @"[A-F]")) {
+					return Systems.Hexadecimal;
+				}
+			}
+			if (Regex.IsMatch(input, @"^(0B)?[01]+$")) { return Systems.Binary; }
+			if (Regex.IsMatch(input, @"^[0-7]+$")) { return Systems.Octal; }
+			if (Regex.IsMatch(input, @"^\d+$")) { return Systems.Decimal; }
+			return Systems.Unknown;
+		}
+	}
+}
---
+++ Blazor/Components/Pages/Numbers.razor.cs
@@ -0,0 +1,36 @@
+﻿using System.Numerics;
+using System.Collections.Generic;
+namespace Blazor.Components.Pages {
+	public partial class Numbers {
+		UInt128 input_factorial;
+		BigInteger result_factorial;
+
+		int input_fibonacci;
+		List<int> Result_fibonacci = new List<int> { };
+		string result_fibonacci = string.Empty;
+
+
+		void Factorial() {
+			result_factorial = 1;
+			for (UInt128 i = 1; i <= input_factorial; i++) {
+				result_factorial *= i;
+			}
+		}
+
+		void Fibonacci() {
+			int a = 0, b = 1;
+			Result_fibonacci.Add(a);
+			Result_fibonacci.Add(b);
+			for (int i = 1; i <= input_fibonacci - 1; i++) {
+				int i_temp = a;
+				a = b;
+				b = i_temp + b;
+				Result_fibonacci.Add(b);
+			}
+			string s_temp = string.Join(" - ", Result_fibonacci.ConvertAll(i => i.ToString()));
+			result_fibonacci = string.Empty;
+			result_fibonacci += s_temp;
+		}
+
+	}
+}
